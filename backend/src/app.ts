2b593e8c93d2818a2dd0f14ebbc0f9f52238e762
mridulvaishnav <mriduliiti@gmail.com>
2026-04-03 10:32:02 +0530

    add DockerFile and serve frontend in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import path from 'path';
+
 import { clerkMiddleware } from '@clerk/express'
 
 import authRoutes from './routes/authRoutes';
 import chatRoutes from './routes/chatRoutes';
 import messageRoutes from './routes/messageRoutes';
 import userRoutes from './routes/userRoutes';
-import e from 'express';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
@@ -25,5 +26,14 @@ app.use("/api/users",userRoutes)
 
 // error handlers must come after all routes and other middlewares do they can catch errors passed with next() or thrown in async route handlers
 app.use(errorHandler)
+
+//serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+  });
+}
 
 export default app;
